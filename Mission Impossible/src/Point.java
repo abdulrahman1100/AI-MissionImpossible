a5c070e1a905d851bee58f90e269257de681581b
abdulrahman1100 <47214336+abdulrahman1100@users.noreply.github.com>
2020-11-07 17:23:20 +0200

    searchtree2

--- Mission Impossible/src/Point.java
+++ Mission Impossible/src/Point.java
@@ -10,8 +10,27 @@ public class Point {
 		// TODO Auto-generated constructor stub
 	}
 
-	public void setPoint(int x, int y) {
+	public Point setPoint(int x, int y) {
 		this.x = x;
+		this.y = y;
+
+		return this;
+	}
+	
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
 		this.y = y;
 	}
 
--- Mission Impossible/src/generic_search.java
+++ Mission Impossible/src/generic_search.java
@@ -1,7 +1,7 @@
 import java.util.TreeSet;
 
 abstract public class generic_search {
-	
+
 	TreeSet<String> state_space;
 	node initialState;
 	String setOfOperators;
@@ -9,8 +9,8 @@ abstract public class generic_search {
 //	abstract void BFS(node initialNode);
 //	abstract void pathCost();
 //	abstract void goalTest();
-	abstract Point transition(String t,Point p);
+	abstract Point transition(String t, Point p);
+
 	abstract void action(Point p);
-	
 
 }
